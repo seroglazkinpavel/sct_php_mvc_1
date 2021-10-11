f226e28ab7cb6de9ec7c23575b0c26976672edfe
seroglazkinpavel <74137950+seroglazkinpavel@users.noreply.github.com>
2021-10-11 21:39:53 +0400

    Update index.php

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ error_reporting(E_ALL);
 
 spl_autoload_register(function($class){
     $path = str_replace('\\', '/', $class. '.php');
-    if(file_exists($path)) {
+    if (file_exists($path)) {
         require $path;
     }
 });
