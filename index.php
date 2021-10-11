aab39e3d160c504b22f14d7e54a4018c9dde1b1d
seroglazkinpavel <74137950+seroglazkinpavel@users.noreply.github.com>
2021-10-11 22:00:54 +0400

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ use app\core\Router;
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
-spl_autoload_register(function($class){
+spl_autoload_register(function($class) {
     $path = str_replace('\\', '/', $class. '.php');
     if (file_exists($path)) {
         require $path;
