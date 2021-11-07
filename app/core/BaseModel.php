cd0500de3e9fef2627ee34853a18701f757d1c2b
MSI <seroglazkinpavel@yandex.ru>
2021-11-07 22:09:05 +0400

    добавление класса

---
+++ app/core/BaseModel.php
@@ -0,0 +1,63 @@
+<?php
+
+
+namespace app\core;
+
+use PDO;
+use PDOException;
+
+abstract class BaseModel
+{
+    protected $db;
+
+    public function __construct()
+    {
+        $config = require 'app\config\db.php';
+        try {
+            $this->db = new PDO(
+            'mysql:host=' . $config['hostname'] . ';dbname=' . $config['database'],
+                $config['username'],
+                $config['password']
+            );
+        } catch (PDOException $exception) {
+            print 'Ошибка:' . $exception->getMessage() . '<br>';
+            die();
+        }
+    }
+
+    protected function query($sql, $params = [])
+    {
+        $query = $this->db->prepare($sql);
+        if(!empty($params)) {
+            foreach($params as $key=>$val) {
+                $query->bindValue(':' . $key, $val);
+            }
+        }
+        $query->execute();
+       return $query;
+    }
+
+    protected function select($sql, $params = [])
+    {
+        $result = $this->query($sql, $params);
+        return $result->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    protected function inser($sql, $params = [])
+    {
+         $this->query($sql, $params);
+        return (int)$this->db->lastInsertId();
+    }
+
+    protected function update($sql, $params = [])
+    {
+        $query = $this->query($sql, $params);
+        return $query->rowCount();
+    }
+
+    protected function delete($sql, $params = [])
+    {
+        $query = $this->query($sql, $params);
+        return $query->rowCount();
+    }
+}
--- app/core/InitController.php
+++ app/core/InitController.php
@@ -4,7 +4,7 @@
 namespace app\core;
 
 
-class InitController
+abstract class InitController
 {
     public $route;
     public $view;
